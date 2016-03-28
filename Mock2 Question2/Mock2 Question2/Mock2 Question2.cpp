17b382788b7a75d7de0dbc7005c9283a6f87a4df
Mukudzei M Mangena <badeovolente@gmail.com>
2016-03-28 19:18:18 +0200

    Question 2

---
+++ Mock2 Question2/Mock2 Question2/Mock2 Question2.cpp
@@ -0,0 +1,6 @@
+//******Questin 2******
+//Question 2 has 3parts(You do NOT need to include each class in a separate file!)
+//2.1.	Create a virtual class "Polygon" which has one method area().
+//2.2	From class "Polygon",derive a concrete class "Triangle" and "Rectangle".Calculate 
+//		the appropriate area for each class and print it on screen.
+//2.3	Write a driver program which creates an object of each type and demonstrates POLYMORPHISM.
